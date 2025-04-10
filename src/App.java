0c881a3528677cc7254ac11a8f09c197d3aa5224
Vic <vcaridadlopez@gmail.com>
2025-04-10 18:03:25 -0400

    Added data saving system and fixed input crashing.

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,8 +7,26 @@ public class App {
     private final Scanner scnr = new Scanner(System.in);
     private final ArrayList<Expense> expenseList = new ArrayList<>();
 
+
     public void start() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("catalog");
+            Scanner fileScanner = new Scanner(fileInputStream);
+            int expenseCount = fileScanner.nextInt();
+            for (int i = 0; i < expenseCount; i++) {
+                Expense expense = new Expense(
+                    fileScanner.nextFloat(),
+                    fileScanner.next(),
+                    fileScanner.nextLine().substring(1)
+                );
+                expenseList.add(expense);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Could not find existing catalog data, initializing SPENDI with no data...");
+        }
         System.out.println("Welcome to SPENDI the expense tracker.");
+
         mainMenu();
     }
 
@@ -15,8 +35,7 @@ public class App {
         System.out.println("Choose any of these options: \n" +
                 "1.Add Expense 2.Delete Expense 3.Show Summary 4.View All 5.Exit");
 
-        int input = scnr.nextInt();
-        scnr.nextLine();
+        int input = getUserInt();
         if(input == 1) {
             addExpense();
         }
@@ -44,7 +63,7 @@ public class App {
         System.out.println("Enter cost:");
         float cost = 0;
         while (cost <= 0) {
-            cost = scnr.nextFloat();
+            cost = getUserFloat();
             if (cost <= 0) {
                 System.out.println("Cost cannot be negative or 0. Enter cost:");
             }
@@ -72,8 +91,7 @@ public class App {
         System.out.println("Select a category: \n" +
                 "1.Personal 2.Home 3.Business 4.School");
 
-        int categoryInput = scnr.nextInt();
-        scnr.nextLine();
+        int categoryInput = getUserInt();
         if(categoryInput == 1) {
             return "Personal";
         }
@@ -99,7 +117,7 @@ public class App {
 
         System.out.println("Choose an expense to delete: ");
         while (true) {
-            int input = scnr.nextInt();
+            int input = getUserInt();
 
             if (input >= 0 && input < expenseList.size()) {
                 expenseList.remove(input);
@@ -165,7 +183,55 @@ public class App {
     }
 
     public void exit() {
-        System.out.println("Thank you for choosing SPENDI! Goodbye.");
+        try {
+            FileWriter fileWriter = new FileWriter("catalog");
+            fileWriter.write(expenseList.size() + "\n");
+            for (Expense expense: expenseList) {
+                fileWriter.write(
+                    String.format(
+                        "%f %s %s\n",
+                        expense.getCost(),
+                        expense.getCategory(),
+                        expense.getDescription()
+                    )
+                );
+            }
+
+            fileWriter.flush();
+            fileWriter.close();
+            System.out.println("Thank you for choosing SPENDI! Goodbye.");
+        }
+        catch (Exception e) {
+            System.out.println("Sorry, we couldn't save your data, please try again.");
+            mainMenu();
+        }
     }
+
+    public int getUserInt() {
+        try {
+            int num = scnr.nextInt();
+            scnr.nextLine();
+            return num;
+        }
+        catch (Exception e) {
+            scnr.nextLine();
+            System.out.println("Input was not a number, please try again: ");
+            return getUserInt();
+        }
+    }
+
+    public float getUserFloat() {
+        try {
+            float num = scnr.nextFloat();
+            scnr.nextLine();
+            return num;
+        }
+        catch (Exception e) {
+            scnr.nextLine();
+            System.out.println("Input was not a number, please try again: ");
+            return getUserFloat();
+        }
+    }
+
 
 }
